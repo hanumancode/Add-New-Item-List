7d447d799ee81e23f28d6fcd0a208a80e7ad9966
Gareth Jones <garethjones@cybertron.local>
2018-08-13 12:36:25 +0100

    new li list item is added on button click event

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var list = document.getElementById("list");
+var btn = document.getElementById("btn");
 var headline = document.getElementById("new_item_header");
 var listItems = document.getElementById("list").getElementsByTagName("li");
 
@@ -6,6 +8,11 @@ for (i = 0; i < listItems.length; i++) {
 }
 
 function activateItem() {
-    // alert("click detected");
     headline.innerHTML = this.innerHTML;
+}
+
+btn.addEventListener("click", createNewItem);
+
+function createNewItem() {
+    list.innerHTML += "<li>new string</li>";
 }
