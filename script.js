2b9e0ac744bbf68acb1920fc5851f4c63caee86f
Gareth Jones <garethjones@cybertron.local>
2018-08-13 13:33:08 +0100

    updated the activateItem function to remove class of active from all sibling elements

--- script.js
+++ script.js
@@ -4,20 +4,26 @@ var btn = document.getElementById("btn");
 var headline = document.getElementById("new_item_header");
 var listItems = document.getElementById("list").getElementsByTagName("li");
 
-for (i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener("click", activateItem);
-}
+// // loop through the listItems collection and adds an addEventListener for each item
+// for (i = 0; i < listItems.length; i++) {
+//     listItems[i].addEventListener("click", activateItem);
+// }
 
-function activateItem() {
-    headline.innerHTML = this.innerHTML;
+// event delegation = active event handling for elements that might exist in the future 
+// add one single event listener to the parent <ul> element
+list.addEventListener("click", activateItem);
 
-    // remove class of active from all sibling elements
+function activateItem(e) {
+    if (e.target.nodeName == "LI") {
+        headline.innerHTML = e.target.innerHTML;
 
-    for (i = 0; i < listItems.length; i++) {
-        listItems[i].classList.remove("active");
+        // remove class of active from all sibling elements
+        for (i = 0; i < e.target.parentNode.children.length; i++) {
+            e.target.parentNode.children[i].classList.remove("active");
+        }
+        // add class of active to change the css
+        e.target.classList.add("active");
     }
-    // add class of active to change the css
-    this.classList.add("active");
 }
 
 btn.addEventListener("click", createNewItem);
