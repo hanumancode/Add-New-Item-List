f42056af47e4a49e8d22b0270b637d43b79e6147
Gareth Jones <garethjones@cybertron.local>
2018-08-13 13:16:12 +0100

    added background highlight to active item selected using classList.add to add a new class and change the css

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var newItemCounter = 1;
 var list = document.getElementById("list");
 var btn = document.getElementById("btn");
 var headline = document.getElementById("new_item_header");
@@ -9,10 +10,19 @@ for (i = 0; i < listItems.length; i++) {
 
 function activateItem() {
     headline.innerHTML = this.innerHTML;
+
+    // remove class of active from all sibling elements
+
+    for (i = 0; i < listItems.length; i++) {
+        listItems[i].classList.remove("active");
+    }
+    // add class of active to change the css
+    this.classList.add("active");
 }
 
 btn.addEventListener("click", createNewItem);
 
 function createNewItem() {
-    list.innerHTML += "<li>new string</li>";
+    list.innerHTML += "<li>new string " + `${newItemCounter}` + "</li>";
+    newItemCounter++;
 }
